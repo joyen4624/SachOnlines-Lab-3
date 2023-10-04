f5b8ec6266cca5e506689abb1c57202c80ba6d1c
joyen4624 <joyen4624@gmail.com>
2023-10-04 09:10:14 +0700

    Add project files.

---
+++ SachOnlines-Lab-3/Controllers/SachOnlineController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SachOnlines_Lab_3.Models;
+
+
+namespace SachOnline_Lab_3.Controllers
+{
+    
+    public class SachOnlineController : Controller
+    {
+       
+        // GET: SachOnline
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ChuDePartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult NXBPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult SliderPartial()
+        {
+            return PartialView();
+        }
+
+    }
+}
